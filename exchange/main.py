3e26e350b58c98d9b1b19cb4f7e933f041bea263
agomez14 <agomez14@illinois.edu>
2014-09-20 17:15:09 -0400

    Added a shit ton of files

--- exchange/main.py
+++ exchange/main.py
@@ -26,7 +26,7 @@ jinja_environment = jinja2.Environment(loader=
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
-    template = jinja_environment.get_template('homepage.html')
+    template = jinja_environment.get_template('index.html')
     self.response.out.write(template.render())
 
 
